3e4546dfe6edf4a893b66cc83622acbe608dab07
Caleb Crane <ccrane@suse.de>
2023-10-18 23:23:20 -0400

    Fix execution of virt-install-demo.sh script (bsc#1216360)
     - Change paths to virsh and virt-install to refer to the correct
       install location
     - Fix issue when naming VM
     - Fix issue where guest boot would hang while mounting its filesystem
    
    Signed-off-by: Caleb Crane <ccrane@suse.de>

--- container/virt-install-demo.sh
+++ container/virt-install-demo.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-set -exo pipefail
+set -eo pipefail
 
 if [ -z ${CONF} ]; then CONF=/etc/kvm-server.conf; fi
 if [ -z ${DEFAULT_CONF} ]; then DEFAULT_CONF=/etc/default/kvm-server; fi
 
 echo "using ${CONF} as configuration file"
+
+# Check for read only root filesystem
+BIN_INSTALL_PATH=$([[ -n $(awk '$2 ~ /\/$/ && $4 ~ /(^|,)ro($|,)/' /proc/mounts) ]] && echo "/usr/local/bin" || echo "/usr/bin")
 
 check_load_config_file() {
 if [ -f ${CONF} ]; then
@@ -28,17 +31,19 @@ fi
 }
 
 start_default_network() {
-   /usr/bin/virsh net-list --inactive --name | grep -qF "default_network" && /usr/bin/virsh net-start default_network
+   ${BIN_INSTALL_PATH}/virsh net-list --inactive --name | grep -qF "default_network" && ${BIN_INSTALL_PATH}/virsh net-start default_network || echo "default_network already started"
 }
 
-RANDOMSTRING=`openssl rand -hex 5`
-VMNAME=${DOMAIN}_${RANDOMSTRING}
+get_vm_name() {
+   RANDOMSTRING=`openssl rand -hex 5`
+   VMNAME=${DOMAIN}_${RANDOMSTRING}
+}
 
 # ignition is not used right now
 #cp -v VM_config.ign ${DATA}
 
 create_vm() {
-/usr/bin/virt-install \
+${BIN_INSTALL_PATH}/virt-install \
     --connect qemu:///system \
     --import \
     --name ${VMNAME} \
@@ -53,7 +58,7 @@ create_vm() {
     --rng /dev/urandom \
     --vcpu ${VCPU} --memory ${VMMEMORY} \
     --cloud-init \
-    --disk size=${DISKSIZE},backing_store=${BACKING_STORE},backing_format=${BACKING_FORMAT},bus=virtio,cache=none \
+    --disk path=${BACKING_STORE},bus=virtio,cache=none \
     --graphics vnc,listen=0.0.0.0,port=5950
 
 # ignition needs another variant of image
@@ -62,12 +67,6 @@ create_vm() {
 
 check_load_config_file
 get_disk_image
+get_vm_name
 start_default_network
 create_vm
-cat <<EOF 
- To connect to the VM in console mode:
-virsh console ${VMNAME}
-
- To detach from the console:
-crtl + ]
-EOF
